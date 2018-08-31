8c3a7730adb9e366f963a8d372f5581265f8ce73
unknown <zxwang@sunnyoptical.com>
2018-08-31 16:56:44 +0800

    add dist

---
+++ dist/config/ProgressConfig.js
@@ -0,0 +1,8 @@
+export default{
+  zIndex: 2000, // progress z-index
+  top: 0, // position fixed top
+  speed: 300, // progress speed
+  color: '#9B30FF', // color
+  size: 2, // progress size
+  className: '' // progress custom class
+}
